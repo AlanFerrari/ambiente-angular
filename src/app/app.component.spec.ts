505a9a47a3147a8b8d920a31c263d2d83a6ec3b3
Alan Ferrari Silva <alanferrari33@gmail.com>
2022-08-10 01:35:31 -0300

    aprendendo sobre o ambiente angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'meu-segundo-app'`, () => {
+  it(`should have as title 'ambiente-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('meu-segundo-app');
+    expect(app.title).toEqual('ambiente-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('meu-segundo-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ambiente-angular app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'meu-segundo-app';
+export class AppComponent implements OnInit {
+  cont = 0;
+  text = '';
+
+  pessoas = [
+    {
+      nome: 'Alan',
+      sobrenome: 'Ferrari'
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'José'
+    },
+    {
+      nome: 'Pamela',
+      sobrenome: 'Nascimento'
+    },
+    {
+      nome: 'Marcio',
+      sobrenome: 'Donato'
+    }
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    let intervalo = setInterval(() => {
+      this.cont++;
+      if (this.cont === 10) {
+        clearInterval(intervalo);
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em mim', nome)
+  }
 }
